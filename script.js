5dd231942044676fb5c27bfdffbf3ff010307de1
GJGH16 <gutierrezgjgh@gmail.com>
2025-02-14 11:31:30 -0400

    javaScript

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+const cancion = {
+
+    _id: '1',
+    author: 'elvis crespo',
+    title: 'Give me the power',
+    duracion: '5:21',
+    Audio: {
+
+        id: '',
+        url: '',
+        filename: '',
+
+    },
+
+    image: {
+
+        id: '',
+        url: '',
+        filename: '',
+
+    }
+
+
+
+}
+
+
+function CrearComponentCancion(song) {
+
+    const div = document.createElement('div')
+    div.setAttribute('class', 'cancion')
+
+    div.innerHTML = `
+    
+                    <img src="assets/cancion.jpg" alt="40px">
+
+                    <div>
+
+                        <h2>${song.title}</h2>
+                         <p>${song.author}</p>
+
+                    </div>
+
+    `
+
+    return div
+
+
+}
+
+
+console.log(
+
+
+    CrearComponentCancion(cancion)
+
+)
